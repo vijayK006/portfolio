de356e2d097dac1696690b2f0f82fc1c7115858f
vijayK006 <vijaykale7052@gmail.com>
2024-02-10 10:08:49 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+'use client'
+import { useState } from 'react';
+import { Container } from 'react-bootstrap'
+const page = () => {
+
+
+  return (
+    <>
+    <Container>
+    <h1>Welcome Next js</h1> 
+
+
+
+    </Container>
+    </>
+  )
+}
+
+export default page
