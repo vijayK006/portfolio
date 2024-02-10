df4543da34593daa9441ebdebddda2f9b9ef6ef6
vijayK006 <vijaykale7052@gmail.com>
2024-02-10 19:50:39 +0530

    uploading banner section

--- components/Banner.js
+++ components/Banner.js
@@ -1,34 +1,29 @@
 import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap';
-import bannerima1 from '../public/bannerimg-1.png'
 
 const Banner = () => {
   return (
     <>
       <div className='banner'>
-        <Container >
-          <Row>
-            <Col md={6}>
+     
               <div className='content'>
-                <h5>Hi there, I am</h5>
-                <h1>Vijay S Kale</h1>
+                <h5 className='mb-4'>Hi there, I am</h5>
+                <h1 className='mb-3'>Vijay S Kale</h1>
                 <h6>Full Time Frontend Web Developer</h6>
+
+                <p>Open To Collaborations & Opportunities | HTML5 | CSS3 | 
+                Bootstrap5 | JavaScript | React.js | Next.js | Editing Web Templates.</p>
+
+<div className='d-flex align-items-center gap-4 pt-3'>
+<Link href='/' className='btn-banner light'>Contact me</Link>
+<Link href='/' className='btn-outline-banner light'>Download CV</Link>
+</div>
               </div>
-            </Col>
+           
 
-            <Col md={6}>
-              <Image src={bannerima1} alt='banner-image'  />
         <div className='cicrle-bg'/>
-
-            </Col>
-
-
-          </Row>
-
-        </Container>
-
-
 
       </div>
     </>
