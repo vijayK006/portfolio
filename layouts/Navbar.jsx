cbcc1b0ed5fe44c9050f5fd8d0ff59a2b1a4f5f0
vijayK006 <vijaykale7052@gmail.com>
2024-02-18 20:48:49 +0530

    responsiveness issue fixed

--- layouts/Navbar.jsx
+++ layouts/Navbar.jsx
@@ -2,8 +2,15 @@ import Image from 'next/image'
 import React from 'react'
 import logo from '../public/logo/logo.png';
 import Link from 'next/link';
+import { CgMenuRight } from "react-icons/cg";
+
 
 const Navbar = () => {
+  const togglemenu=()=>{
+const menu = document.getElementById('menu');
+menu.classList.toggle('openmenu')
+  }
+
   return (
     <>
      <nav>
@@ -12,7 +19,7 @@ const Navbar = () => {
             <h5 className='margin-0 '>ALE</h5>
         </div>
 
-        <ul className='nav_items'>
+        <ul className='nav_items' id='menu'>
 <Link href="/"><li className='nav_list'>Home</li></Link> 
 <Link href="#aboutme"><li className='nav_list'>About me</li></Link> 
 <Link href="/"><li className='nav_list'>Projects</li></Link> 
@@ -20,6 +27,10 @@ const Navbar = () => {
 
 <Link href='/vijay-cv-2024.pdf' target='_black' download="vijay-cv-2024.pdf" className='btn-banner light'>Download my CV</Link>
         </ul>
+
+        <div className='menu' onClick={togglemenu}>
+        <CgMenuRight  className='icon'/>
+        </div>
      </nav> 
     </>
   )
